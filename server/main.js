ba6f5b8d7ae0c3c134fde356bf0ddfa8533c17d0
skaru2000 <skaru2002@naver.com>
2021-11-25 23:00:35 +0900

    Update main.js

--- server/main.js
+++ server/main.js
@@ -1,10 +1,40 @@
- import express, { Router } from 'express';
- const app = express();
+import express from 'express';
+import fs, { readFile } from 'fs';
+import fsAsync from 'fs/promises';
+
+const app = express();
 
 app.use(express.json())
 
+app.get('/file1', (req, res) => {
+    // try {
+    //     const data = fs.readFileSync('/file.txt')
+    // }catch (error) {
+    //     res.status(404).send('Filenotfound')
+    // }
+
+    fs.readFile('/file1.txt', (err,data) => {
+        if(err){
+            res.status(404).send('Filenotfound')
+        }
+    })
+})
+
+app.get('/file2', (req, res) => {
+    fsAsync.readFile('/file.txt').catch((error) => res.status(404).send('FilenotFound'));
+})
+
+app.get('/file3', async (req, res) => {
+    const data = await fsAsync.readFile('/file.txt')
+})
+
 app.post('/', (req,res,next) => {
     console.log(req.body);
+}) 
+
+app.use((error, req, res, next) =>{
+    console.error(error);
+    res.status(500).json({message: 'Something went wrong'})
 })
 
 app.listen(8080);
